13bb661709ef022d2c33f035d43074035882fc3d
Hem Pun <hemthegentlmen@gmail.com>
2023-04-10 19:01:52 +0545

    Added Styling and Started Exploring Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Pet from './components/Pet'
+import SearchParams from './components/SearchParams'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Adopt Me !</h1>
-      <Pet name='Luna' animal='dog' breed='don' />
-      <Pet name='Sufi' animal='cat' breed='haha' />
-      <Pet name='Luna' animal='dog' breed='don' />
+      <SearchParams />
     </div>
   )
 }
