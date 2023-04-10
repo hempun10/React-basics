afba96ac72447ffaa0f1df50a372d386e6789a94
Hem Pun <hemthegentlmen@gmail.com>
2023-04-10 19:37:02 +0545

    Making map concept clear

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const location = 'Seattle , WA'
+    const [location, setLocation] = useState('')
+    const [animal, setAnimal] = useState('')
+
     return (
         <>
             <div className='search-params'>
                 <form>
-                    <label htmlFor='location'>
+                    <label htmlFor='location' onChange={e => setLocation(e.target.value)}>
                         location
                         <input id='location' value={location} placeholder='location'></input>
                     </label>
+                    <label htmlFor='animal'>
+                        Animal
+                        <select id='animal' value={animal} onChange={e => setAnimal(e.target.value)}>
+                            {ANIMALS.map((animal) => <option key={animal}>{animal}</option>)}
+                        </select>
+                    </label>
+
                     <button>Submit</button>
                 </form>
             </div>
         </>
     );
+
 }
 
 export default SearchParams;
