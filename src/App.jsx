ff41018b6458dcd1359398dae1599ccb29e9a577
Hem Pun <hemthegentlmen@gmail.com>
2023-04-11 19:38:01 +0545

    Learned about react router and Learned why to use Link than a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
 import './App.css'
-import Pet from './components/Pet'
+import Details from './components/Details'
 import SearchParams from './components/SearchParams'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Adopt Me !</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Adopt Me !</h1>
+        <Routes>
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/' element={<SearchParams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
