db9ee1286ada7559f370416a8f1cee4ce13e1f06
Hem Pun <hemthegentlmen@gmail.com>
2023-04-12 07:08:21 +0545

    Learning more on refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import './App.css'
 import Details from './components/Details'
 import SearchParams from './components/SearchParams'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
-function App() {
-  // const [count, setCount] = useState(0)
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      scaleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
+function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header>
-          <Link to='/' > Adopt Me </Link>
-        </header>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <header>
+            <Link to='/' > Adopt Me </Link>
+          </header>
 
-        <Routes>
-          <Route path='/details/:id' element={<Details />} />
-          <Route path='/' element={<SearchParams />} />
-        </Routes>
-      </div>
+          <Routes>
+            <Route path='/details/:id' element={<Details />} />
+            <Route path='/' element={<SearchParams />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
